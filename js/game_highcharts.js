48c3188c3c876cb9a57f6909aa37820e06a5e1d6
R0onen <erni100105@gmail.com>
2024-11-19 17:22:54 +0500

    adding highcharts into games page

---
+++ js/game_highcharts.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function () {
+  Highcharts.chart('chart-container', {
+      chart: {
+          backgroundColor: '#020307',
+          type: 'bar'
+      },
+      title: {
+          text: 'Top-Rated Games',
+          style: {
+              color: '#ffffff',
+              fontSize: '20px'
+          }
+      },
+      xAxis: {
+          categories: [
+              'Grand Theft Auto V', 
+              'Minecraft', 
+              'The Elder Scrolls V: Skyrim', 
+              'The Witcher 3: Wild Hunt', 
+              'Assassins Creed IV: Black Flag', 
+              'God of War', 
+              'Horizon Zero Dawn', 
+              'Outer Wilds'
+          ],
+          labels: {
+              style: {
+                  color: '#ffffff',
+                  fontSize: '14px'
+              }
+          },
+          title: {
+              style: {
+                  color: '#ffffff'
+              }
+          }
+      },
+      yAxis: {
+          title: {
+              text: 'Rating (out of 10)',
+              style: {
+                  color: '#ffffff',
+                  fontSize: '14px'
+              }
+          },
+          labels: {
+              style: {
+                  color: '#ffffff', 
+                  fontSize: '14px'
+              }
+          }
+      },
+      legend: {
+          itemStyle: {
+              color: '#ffffff', 
+              fontSize: '12px'
+          }
+      },
+      series: [{
+          name: 'Ratings',
+          data: [9, 8.7, 9.3, 10, 8.5, 9.4, 8.3, 8.5],
+          color: '#007bff'
+      }]
+  });
+});
